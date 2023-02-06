c07756b854aba1da5beb129fbbf6ce8c56c5cb1d
anhtudo97 <anhtudo97@gmail.com>
2023-02-07 00:06:41 +0700

    feat(update): components

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,21 @@
 import Comments from './Comments';
 import Discover from './Discover';
 import Footer from './Footer';
+import LikeButton from './LikeButton';
+import Navbar from './Navbar';
+import NoResult from './NoResults';
+import Sidebar from './Sidebar';
+import SuggestedAccounts from './SuggestedAccounts';
+import VideoCard from './VideoCard';
 
-export { Comments, Discover, Footer };
+export {
+  Comments,
+  Discover,
+  Footer,
+  LikeButton,
+  Navbar,
+  NoResults,
+  Sidebar,
+  SuggestedAccounts,
+  VideoCard,
+};
